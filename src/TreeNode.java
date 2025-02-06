9307b071a1453b2d714bbd1cff118e92d1c524e3
TavSingh <tavparsadsingh2@gmail.com>
2025-02-06 14:48:48 -0800

    Worked on TreeNode and Traversal

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,14 @@
 public class TreeNode {
-
+    // Fields(instance var)
+    int value;
+    TreeNode left;
+    TreeNode right;
   
+    // Constructors
+    public TreeNode (int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
 }
